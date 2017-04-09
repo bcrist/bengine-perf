f18fafd34cfbf1acdbb97d17ce0b419e7e3c4273
Ben Crist <ben@magicmoremagic.com>
2017-04-09 18:17:38 -0500

    versioning

---
+++ include/version.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef BE_PERF_VERSION_HPP_
+#define BE_PERF_VERSION_HPP_
+
+#include <be/core/macros.hpp>
+
+#define BE_PERF_VERSION_MAJOR 0
+#define BE_PERF_VERSION_MINOR 1
+#define BE_PERF_VERSION_REV 1
+
+/*!! include('common/version', 'be::perf') !! 6 */
+/* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
+#define BE_PERF_VERSION (BE_PERF_VERSION_MAJOR * 100000 + BE_PERF_VERSION_MINOR * 1000 + BE_PERF_VERSION_REV)
+#define BE_PERF_VERSION_STRING "be::perf " BE_STRINGIFY(BE_PERF_VERSION_MAJOR) "." BE_STRINGIFY(BE_PERF_VERSION_MINOR) "." BE_STRINGIFY(BE_PERF_VERSION_REV)
+
+/* ######################### END OF GENERATED CODE ######################### */
+
+#endif
