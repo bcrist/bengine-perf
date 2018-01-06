24313f0c25c4a91cd895cfde1c0a7ae6d8fae41e
Ben Crist <ben@magicmoremagic.com>
2018-01-06 01:03:10 -0600

    windows sdk version bump

--- include/version.hpp
+++ include/version.hpp
@@ -6,7 +6,7 @@
 
 #define BE_PERF_VERSION_MAJOR 0
 #define BE_PERF_VERSION_MINOR 1
-#define BE_PERF_VERSION_REV 6
+#define BE_PERF_VERSION_REV 7
 
 /*!! include('common/version', 'be::perf') !! 6 */
 /* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
